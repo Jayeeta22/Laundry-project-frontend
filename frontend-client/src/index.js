f74d6deca5736ce5a662e8e99bed9a61876b8571
Jayeeta22 <117907901+Jayeeta22@users.noreply.github.com>
2022-11-26 09:38:14 +0530

    header and footer created

--- frontend-client/src/index.js
+++ frontend-client/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Footer from './components/Footer/footer';
-
+// import Register from './components/Register/register';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Footer/>
+    {/* <Register/> */}
   </React.StrictMode>
 );
 
