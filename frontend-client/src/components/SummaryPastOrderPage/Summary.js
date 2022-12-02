45a1a933872f9d691078aafe8488324699c65507
Suneelkuma <sunil24195@gmail.com>
2022-12-02 16:30:14 +0530

    added pastorder Summary.js file with code

--- frontend-client/src/components/SummaryPastOrderPage/Summary.js
+++ frontend-client/src/components/SummaryPastOrderPage/Summary.js
@@ -7,6 +7,7 @@ function Summary(props) {
     // console.log("summary popup");
     // console.log(props.order);
     
+    
     // axios.get(`http://localhost:5000/orders`, {
     //         headers: {
     //             Authorization: 'Bearer ' + token
